1e445e770579775c7dc53a8b83ef3f996a3ff2dd
kylestech95 <kylestech95@outlook.com>
2023-11-22 11:43:20 -0500

    experiemnting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/**/*.{html,js}",
-          "./build/*.{html,js}"],
+  content: ["./build/**/*.{html,js}"],
     theme: {
     extend: {},
   },
